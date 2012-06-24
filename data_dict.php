2430bc896e77bfcbf5da61764ae5e99127f85ea2
ratno <ratno@knoqdown.com>
2012-06-24 10:48:36 +0700

    add data dictionary view

---
+++ data_dict.php
@@ -0,0 +1,43 @@
+<?php
+include_once 'spyc.php';
+include_once 'func.php';
+
+$yml = Spyc::YAMLLoad("ws.yml");
+?>
+<html>
+  <head>
+    <title>Data Dictionary for <?php $yml['ws']['unit'] ?></title>
+  </head>
+  <body>
+    
+    <?php
+    foreach ($yml['services'] as $service_name => $service) {
+      echo "<h1>$service_name:</h1>";
+      echo "<b>".$service['desc']."</b>";
+      echo "<h2>Parameter Input Service:</h2>";
+      echo "<table border=1 cellpadding=5 cellspacing=0>";
+      echo "<tr>";
+      echo "<th>Parameter Input</th>";
+      echo "<th>Tipe</th>";
+      echo "<th>Info</th>";
+      echo "<tr>";
+      foreach($service['input'] as $input_key=>$input_opt){
+        echo "<tr>";
+        echo "<td>".$input_key."</td>";
+        echo "<td>".$input_opt['type']."</td>";
+        echo "<td>".$input_opt['info']."</td>";
+        echo "<tr>";
+      }
+      echo "</table>";
+      
+      echo "<h2>Output Fields:</h2>";
+      echo "<ul>";
+      foreach($service['fields'] as $field){
+        echo "<li>$field</li>";
+      }
+      echo "</ul>";
+      echo "<hr />";
+    }
+    ?>
+  </body>
+</html>
